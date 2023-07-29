a64188c9052d84dd7fcf2ade3d1e4cb6f2560c19
Lucas Carvalho <lucascarvalho_direito@hotmail.com>
2023-07-29 12:51:52 -0300

    css quase completo

---
+++ 1-fundamentos/3-front-end/1-3-casa-de-cambio/src/main.js
@@ -0,0 +1,22 @@
+import './style.css';
+
+
+
+// const API_URL = `https://api.exchangerate.host/latest?base=${moeda}`;
+
+
+
+
+
+
+
+
+
+
+
+
+// .catch((erro) => Swal.fire({
+//   title: 'Error!',
+//   text: 'Do you want to continue',
+//   icon: 'error',
+//   confirmButtonText: 'Cool'})
